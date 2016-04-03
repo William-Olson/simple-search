f102b6543f5579939a4309dc8714376e22140eed
William Olson <willko747@gmail.com>
2016-04-02 20:11:17 -0500

    opt badges + rerank btn & options btn relocations

--- cse/client/js/main-ctrl.js
+++ cse/client/js/main-ctrl.js
@@ -1,10 +1,11 @@
 'use strict';
 
 // @ngInject
-module.exports = ($scope) => {
+module.exports = ($scope, mainSvc) => {
 
   // selects search box with cursor
   $scope.focusSrch = () => { $('#srchbox').focus(); };
+  $scope.opts = mainSvc.getConfig().current;
 
   //focus on search box on page load
   setTimeout($scope.focusSrch, 100);

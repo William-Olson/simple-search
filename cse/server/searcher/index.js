bd812e24d1e5de6391b51bfb7c23dd9db624db57
Will Olson <willko747@gmail.com>
2016-04-03 12:50:43 -0500

    bumping up result count to 20

--- cse/server/searcher/index.js
+++ cse/server/searcher/index.js
@@ -18,10 +18,35 @@ var googleSearch = new GoogleSearch({
 // module setup
 var mod = {};
 mod.search = (term, cb) => {
-  googleSearch.build({
-    q: term,
-    num: 10 
-  }, cb); // cb(err, rsp);
+  let req = {
+  	q: term,
+  	num: 10
+  };
+  // send query
+  googleSearch.build(req, (err1, res1) => {
+
+  	if(err1) cb(err1);
+      //now try to grab the next page
+      if(res1.searchInformation && res1.searchInformation.totalResults > 10) {
+
+      	//perform the nextPage query
+        req.start = 11;
+        googleSearch.build(req, (err2, res2) => {
+          if(err2) cb(err2);
+
+          //concat results and return them
+          if(res2.items)
+          	res1.items = res1.items.concat(res2.items);
+
+          //return response 1 with or with-out res2 items
+          cb(null, res1);
+        });
+
+      } else{
+      	cb(null, res1);
+      }
+
+  });
 };
 
 

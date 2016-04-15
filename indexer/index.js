ff8579b64aadfd59d8d26bb55be1ca0548763b4b
Will Olson <willko747@gmail.com>
2016-04-15 15:06:26 -0500

    elastic default ports

--- indexer/index.js
+++ indexer/index.js
@@ -3,7 +3,7 @@
 let elastic = require('elasticsearch');
 let data;
 
-// sanity check, bail on bad data
+// sanity check, bail on no data
 try {
   data = require('/data/data.json');
 } catch(err) {
@@ -14,8 +14,8 @@ try {
 
 //create elastic client
 let client = elastic.Client({
-  host: 'ss_elastic:9000',
-  log: 'trace'
+  host: 'ss_elastic:9200'
+  // , log: 'trace'
 });
 
 // build a body array for indexing in bulk fashion
@@ -23,7 +23,8 @@ let bodyArr = [];
 data.forEach((d) => {
   bodyArr.push({ index:  { _index: 'main', _type: 'repo' } });
   bodyArr.push(d);
-  console.log('setting up index for: ', d.owner.handle, '/', d.name);
+  console.log('setting up (', d.owner.handle, ', ', d.name,
+              ') for bulk indexing');
 });
 
 // do indexing
@@ -31,6 +32,7 @@ client.bulk({
   body: bodyArr
 }, (err, resp) => {
   if(err){
+    console.error('AN ERROR HAS OCCURRED');
     console.error(err);
   } else{
     console.log('BULK INDEXING SUCCESSFULL');
